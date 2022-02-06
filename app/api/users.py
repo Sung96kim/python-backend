3485b80cd7f127b8c63fdf8c1834c8cb400907a9
Sung <sung96kim@gmail.com>
2022-02-06 18:19:39 -0500

    ADD password hashing for user

--- app/api/users.py
+++ app/api/users.py
@@ -1,5 +1,6 @@
-from fastapi import Depends, APIRouter, status, HTTPException
+from fastapi import Depends, APIRouter, status, HTTPException, Response
 from sqlalchemy.orm import Session
+from ..utils import hash_pwd
 from ..schema import GetUser, CreateUser, UpdateUser
 from ..db_init import get_db
 from ..db.models import User
@@ -20,6 +21,8 @@ async def get_user(user_id: int, db: Session = Depends(get_db)):
 @router.post("/user")
 async def create_user(user: CreateUser, db: Session = Depends(get_db)):
     
+    hashed_pwd = hash_pwd(user.password)
+    user.password = hashed_pwd
     created_user = User(**user.dict())
     db.add(created_user)
     db.commit()
@@ -46,3 +49,19 @@ async def update_user(user_id: int, update_user: UpdateUser, db: Session = Depen
     db.commit()
     
     return found_user.first()
+
+@router.delete("/user/{user_id}")
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    
+    found_user = db.query(User).filter(User.id == user_id)
+    
+    user = found_user.first()
+    
+    if user == None: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User does not exist")
+
+    found_user.delete(synchronize_session=False)
+    db.commit()
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
